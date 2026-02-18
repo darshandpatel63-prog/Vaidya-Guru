e9706dfb14b6755d6b14faf33b77e4eebd516fa7
darshandpatel63-prog <darshandpatel63@gmail.com>
2026-02-18 18:40:56 +0530

    Fix import statement for PrivacyPolicy component

--- App.tsx
+++ App.tsx
@@ -9,7 +9,7 @@ import { Scheduler } from './components/Scheduler';
 import { AdminPanel } from './components/AdminPanel';
 import { DoctorConnect } from './components/DoctorConnect';
 import { MaterialLibrary } from './components/MaterialLibrary';
-import { PrivacyPolicy }from './components/PrivacyPolicy';
+import  PrivacyPolicy from './components/PrivacyPolicy';
 
 import { MOCK_BOOKS } from './constants';
 import { Book, User, CourseLevel, Language, Role, Gender, MedicalField, DailyQuote } from './types';
@@ -220,6 +220,8 @@ const MainApp: React.FC = () => {
     { id: 'schedule', label: t.schedule, icon: <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.5" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z" /> },
     { id: 'connect', label: t.connect, icon: <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.5" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" /> },
     { id: 'admin', label: t.admin, icon: <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.5" d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" /> },
+    { id: 'privacy', label: "Privacy", icon: <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2.5" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" /> },
+
   ];
 
   return (
@@ -326,6 +328,8 @@ const MainApp: React.FC = () => {
         {activeTab === 'admin' && <AdminPanel user={user} />}
         {activeTab === 'material' && <MaterialLibrary user={user} />}
         {activeTab === 'profile' && <Profile />}
+        {activeTab === 'privacy' && <PrivacyPolicy />}
+        
       </main>
 
       {/* Bottom Ad Banner */}
